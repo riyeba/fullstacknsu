a6b446015bb8f1fa14d2818f9c5f8757dfdba19a
Akinpennu Taiwo <moriyebas@gmail.com>
2024-02-13 08:51:07 +0300

    four

--- nsu_project/settings.py
+++ nsu_project/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = 'django-insecure-0hd@37u&px3^(@cugjq(uyeuwsigfr+p)u3cqg4v6=k)jn&cof
 
 
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1","nsu-ksu-app.onrender.com"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","nsu-ksu-app.onrender.com","fullstackfa.onrender.com"]
 
 
 # Application definition
