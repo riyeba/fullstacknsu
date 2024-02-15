5833f5c604ad799bbacdfbba435810dcb472d837
Akinpennu Taiwo <moriyebas@gmail.com>
2024-02-15 11:48:20 +0300

    mmm

--- nsuapp/serializers.py
+++ nsuapp/serializers.py
@@ -6,6 +6,6 @@ from .models import Info
 class InfoSerializer(serializers.ModelSerializer):
   class Meta:
     model=Info
-    fields=['id','name_text','nick_text','department_text','phone_number','building_number','room_number','photo',]
+    fields=['id','name_text','nick_text','department_text','phone_number','building_number','room_number']
     
   
